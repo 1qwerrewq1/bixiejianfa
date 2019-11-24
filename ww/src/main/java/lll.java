08c885c66aa67e80e0c1964465142a9d65d69ddc
yaobing <1064124677@qq.com>
2019-11-24 22:59:39 +0800

    Merge branch 'dev' of https://github.com/1qwerrewq1/bixiejianfa into dev
    # Conflicts:
    #	ww/src/main/java/lll.java

--- ww/src/main/java/lll.java
+++ ww/src/main/java/lll.java
@@ -6,19 +6,25 @@
 
 public class lll {
 
-    public static void main(String[] args) {
-        System.out.println("hellowrold");
 
+    public static void main(String[] args) {
+        System.out.println("00000000000001");
+        System.out.println("hellowrold");
+//============================
         System.out.println("00000000000002");
         System.out.println("hellowrold2");
-        
-        
-        
+
+
+
         System.out.println("00000000000004");
         System.out.println("hellowrold2");
-        
-        
-        
+
+
+
+
+        System.out.println("00000000000003");
+        System.out.println("hellowrold3");
+
     }
 
 }
